53bc4a00283ed0d99e7ea17c349e530b72b38fb9
danilofumuso <danilofumuso@gmail.com>
2024-12-04 16:57:28 +0100

    creazione creata!

--- src/main/java/it/epicode/gestione_eventi/test/MainDAO.java
+++ src/main/java/it/epicode/gestione_eventi/test/MainDAO.java
@@ -2,8 +2,10 @@ package it.epicode.gestione_eventi.test;
 
 import com.github.javafaker.Faker;
 import it.epicode.gestione_eventi.dao.EventoDAO;
-import it.epicode.gestione_eventi.entity.Evento;
-import it.epicode.gestione_eventi.entity.EventoEnum;
+import it.epicode.gestione_eventi.dao.LocationDAO;
+import it.epicode.gestione_eventi.dao.PartecipazioneDAO;
+import it.epicode.gestione_eventi.dao.PersonaDAO;
+import it.epicode.gestione_eventi.entity.*;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
@@ -15,21 +17,55 @@ public class MainDAO {
 
     public static void main(String[] args) {
 
-        EntityManagerFactory emf= Persistence.createEntityManagerFactory("unit-jpa");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("unit-jpa");
 
-        EntityManager em= emf.createEntityManager();
+        EntityManager em = emf.createEntityManager();
 
-        Faker faker= new Faker(new Locale("it"));
+        Faker faker = new Faker(new Locale("it"));
 
-        EventoDAO eventoDAO= new EventoDAO(em);
-        Evento evento= new Evento();
+        //Persona
+        PersonaDAO personaDAO = new PersonaDAO(em);
+        Persona persona = new Persona();
+        persona.setNome("Andrea");
+        persona.setCognome("La Ventura");
+        persona.setEmail("andreafocoso@gmail.com");
+        persona.setDataDiNascita(LocalDate.of(2003, 01, 15));
+        persona.setSesso(SessoEnum.M);
+//        persona.setListaPartecipazioni();
+
+        personaDAO.insertPersona(persona);
+
+        //Location
+        LocationDAO locationDAO = new LocationDAO(em);
+        Location location = new Location();
+
+        location.setNome("Palasport");
+        location.setCitta("Milano");
+
+        locationDAO.insertLocation(location);
+
+        //Evento
+        EventoDAO eventoDAO = new EventoDAO(em);
+        Evento evento = new Evento();
         evento.setTitolo("Epicode Party");
-        evento.setDataEvento(LocalDate.of(2024,12,11));
+        evento.setDataEvento(LocalDate.of(2024, 12, 11));
         evento.setDescrizione("Super mega party epicode,sarà uno sballo!");
         evento.setTipoEvento(EventoEnum.PUBBLICO);
         evento.setNumeroMassimoPartecipanti(1000);
+        evento.setLocation(location);
+//        evento.setListaPartecipazioni();
 
         eventoDAO.insertEvento(evento);
+
+        //Partecipazione
+        PartecipazioneDAO partecipazioneDAO = new PartecipazioneDAO(em);
+        Partecipazione partecipazione = new Partecipazione();
+
+        partecipazione.setEvento(evento);
+        partecipazione.setPersona(persona);
+        partecipazione.setStato(StatoPartecipazioneEnum.CONFERMATA);
+
+        partecipazioneDAO.insertPartecipazione(partecipazione);
 
         em.close();
     }
